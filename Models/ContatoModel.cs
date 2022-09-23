da284ecbf5b159c95f90324108a387781287ead8
Vanessa Soares <vanessa.carvalhal@gmail.com>
2022-09-23 17:41:25 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ContatoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_cadastro.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string CPF { get; set; }
+        public string Dia { get; set; }
+        public string Mes { get; set; }
+        public string Ano { get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# app-cadastro
+Estudos ASP.NET
